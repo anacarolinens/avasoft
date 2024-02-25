4617a56cf69739df16ed1b8e98037041ee89f05d
Anderson Dantas Santos <netodantas3@gmail.com>
2024-02-25 18:54:48 -0500

    ajustes paginas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,47 @@
+<!-- App.vue -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <component :is="currentComponent" @loginSuccess="handleLoginSuccess" @logout="handleLogout" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import LoginPage from './components/LoginPage.vue';
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    LoginPage,
+    HomePage,
+  },
+  data() {
+    return {
+      showLoginPage: true,
+    };
+  },
+  computed: {
+    currentComponent() {
+      return this.showLoginPage ? 'LoginPage' : 'HomePage';
+    },
+  },
+  methods: {
+    handleLoginSuccess() {
+      // Lógica para lidar com o sucesso do login
+      console.log('Login bem-sucedido. Redirecionando para HomePage...');
+      this.showLoginPage = false;
+    },
+    handleLogout() {
+      // Lógica para lidar com o logout
+      console.log('Logout realizado. Redirecionando para LoginPage...');
+      this.showLoginPage = true;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Seus estilos aqui */
 </style>
---
+++ frontend/src/components/LoginPage.vue
@@ -0,0 +1,42 @@
+<template>
+
+
+    <div>
+      <h1>Login Page</h1>
+    
+      <form action="">
+
+      <label for="name"></label>
+      <input type="text" id="name" placeholder="usuario" v-model="name">
+
+      <label for="pass"></label>
+      <input type="password" id="pass" placeholder="pass">
+
+      <button @click="handleLogin">Login</button>
+
+      </form>
+
+    </div>
+
+
+
+
+
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+  handleLogin() {
+    // Lógica de login aqui
+
+    // Emite o evento loginSuccess para notificar o componente pai (App.vue) sobre o sucesso do login
+    this.$emit('loginSuccess');
+  }
+}
+  }
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos para esta página, se necessário */
+  </style>
