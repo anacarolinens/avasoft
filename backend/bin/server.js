793040a805032f22dc79b11a0be23425ba171f47
Ana Caroline <85416744+anacarolinens@users.noreply.github.com>
2024-02-27 22:16:02 -0500

    feat: Create models and the professional controller

--- backend/bin/server.js
+++ backend/bin/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("../database/db");
 const User = require("../models/user");
+const Professional = require("../models/professional");
 
 require("dotenv").config();
 
@@ -22,7 +23,7 @@ app.use((req, res, next) => {
 
 //CRUD routes
 app.use("/users", require("../routes/userRoute"));
-
+app.use("/professionals", require("../routes/professionalRoute"));
 
 //error handling
 app.use((error, req, res, next) => {
---
+++ backend/models/professional.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize')
+const database = require('../database/db');
+const User = require('./user');
+
+
+const Professional = database.define('profissional', {
+  id_professional: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  registry_professional: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  experience: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: true
+  },
+  userName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  confirmPassword: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  id_user: { 
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {         
+      model: User,        
+      key: 'id_user'    
+    }
+  }
+});
+
+module.exports = Professional
