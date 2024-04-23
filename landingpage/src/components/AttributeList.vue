64bd1cc61b483d41b8f9efc1bdeec0b03b5a4ee0
Ana Caroline <caroline.ana.n.s@gmail.com>
2024-04-23 11:41:44 -0500

    feat: update attributes with hover

--- landingpage/src/components/AttributeList.vue
+++ landingpage/src/components/AttributeList.vue
@@ -4,9 +4,10 @@
             <img class="w-3/4" :src="imagePath('notebook.png')" alt="Notebook com demonstração do">
         </div>
 
-        <div class="flex flex-wrap justify-center text-center mt-16 mb-28" style="min-width: 320px;">
+        <div class="flex flex-wrap justify-center text-center mt-16 mb-14" style="min-width: 320px;">
             <div v-for="feature in features" :key="feature.title" class="w-full feature-container mb-8" style="max-width: 200px;">
-                <button @click="toggleDescription(feature)" class="feature-button">
+                <div @mouseover="toggleDescription(feature, true)" @mouseleave="toggleDescription(feature, false)"
+                    class="feature-button">
                     <div class="flex flex-col items-center">
                         <div :class="['w-16 h-16 bg-white rounded-full shadow-md flex items-center justify-center', {'w-24 h-24': feature.showDescription}]">
                             <img :src="imagePath(feature.src)" class="feature-icon">
@@ -16,10 +17,10 @@
                             {{ feature.title }}
                         </p>
                     </div>
-                </button>
+                </div>
                 <div v-if="feature.showDescription" class="description-area">
                     <p class="mt-2 text-center">
-                        {{ feature.description}}
+                        {{ feature.description }}
                     </p>
                 </div>
             </div>
@@ -68,17 +69,15 @@ export default {
         };
     },
     methods: {
-        toggleDescription(clickedFeature) {
-            if (this.clickedFeature === clickedFeature) {
-                this.clickedFeature.showDescription = !this.clickedFeature.showDescription;
-            } else {
-                this.features.forEach(feature => {
-                    if (feature !== clickedFeature) {
-                        feature.showDescription = false;
-                    }
+        toggleDescription(feature, show) {
+            if (show) {
+                this.features.forEach(f => {
+                    f.showDescription = f === feature;
                 });
-                clickedFeature.showDescription = true;
-                this.clickedFeature = clickedFeature;
+                this.clickedFeature = feature;
+            } else {
+                feature.showDescription = false;
+                this.clickedFeature = null;
             }
         },
         imagePath(fileName) {
