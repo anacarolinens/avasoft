bfd585cc8ac42052f8eb364f107a97302dfcb141
Ana Caroline <caroline.ana.n.s@gmail.com>
2024-05-13 09:15:29 -0500

    fix:  fix  duplicate button

--- landingpage/src/components/DropDown.vue
+++ landingpage/src/components/DropDown.vue
@@ -14,16 +14,15 @@
         <img src="../assets/macos.png" alt="Ícone de download" class="h-5 w-5 ml-2">
       </span>
     </button>
+    <button v-if="isLinux"
+      class="flex items-center justify-center bg-[#FF8139] px-6 py-3 text-white rounded-l-md hover:bg-[#FF5C00]">
+      <span class="flex items-center">
+        Baixar para Linux
+        <img src="../assets/linux.png" alt="Ícone de download" class="h-5 w-5 ml-2">
+      </span>
+    </button>
     <div class="relative">
-      <button
-        class="flex items-center justify-center bg-[#FF8139] px-6 py-3 text-white rounded-l-md hover:bg-[#FF5C00]">
-        <span class="flex items-center">
-          Baixar para Linux
-          <img src="../assets/linux.png" alt="Ícone de download" class="h-5 w-5 ml-2">
-        </span>
-      </button>
-
-      <div id="dropdown" class="dropdown absolute z-50 mt-1 w-max origin-top-right rounded-md shadow-lg right-0">
+      <div id="dropdown" class="dropdown absolute z-50 mt-12 w-max origin-top-right rounded-md shadow-lg right-0">
         <div class="bg-white rounded-md ring-1 ring-black ring-opacity-5">
           <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
             <a href="#" class="block px-8 py-2 text-sm text-gray-700 hover:bg-[#FFD6CE] hover:text-gray-900"
@@ -48,20 +47,20 @@ import { ref } from 'vue';
 const detectOS = () => {
   const userAgent = navigator.userAgent;
   if (userAgent.includes("Win")) {
-    return "Windows";
+    return { name: "Windows", isLinux: false };
   } else if (userAgent.includes("Mac")) {
-    return "MacOS";
+    return { name: "MacOS", isLinux: false };
   } else if (userAgent.includes("Linux")) {
-    return "Linux";
+    return { name: "Linux", isLinux: true };
   } else {
-    return "Outro";
+    return { name: "Outro", isLinux: false };
   }
 };
 
 const os = detectOS();
-const isWindows = ref(os === "Windows");
-const isMacOS = ref(os === "MacOS");
-const isLinux = ref(os === "Linux");
+const isWindows = ref(os.name === "Windows");
+const isMacOS = ref(os.name === "MacOS");
+const isLinux = ref(os.isLinux);
 
 const toggleDropdown = () => {
   const dropdown = document.getElementById("dropdown");
