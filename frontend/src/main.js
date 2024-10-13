10ef99dae77547e3c127f8842651f23877e59a4c
Alexandre Lira <75650952+Ale-Lira@users.noreply.github.com>
2024-10-12 22:56:49 -0500

    feat: google, login

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,15 @@ import router from './router';
 import axios from 'axios';
 import './assets/output.css';
 import "preline/preline";
+import vue3GoogleLogin from 'vue3-google-login'
 
 const app = createApp(App);
 
 app.config.globalProperties.$axios = axios;
+
+app.use(vue3GoogleLogin, {
+    clientId: '901081763009-kpkjo48hqu0k51tihec09tmceij7l5go.apps.googleusercontent.com'
+  })
 
 app.use(router); // Use VueRouter
 app.mount('#app');
