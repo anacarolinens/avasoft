25cde8148eb66f5eca19c945ff2c783c4c9220eb
anacarolinens <caroline.ana.n.s@gmail.com>
2024-10-14 13:33:59 -0500

    Revert ":bug: fix: corrected petirnthistoryservice importation"
    This reverts commit 66d36449401c62869fc408ce73abd6fe27a042ed.

--- backend/src/controllers/assessmentController.js
+++ backend/src/controllers/assessmentController.js
@@ -5,7 +5,7 @@ const Skinfold = require('../models/skinfold');
 const Bmi = require('../models/bmi');
 const BodyComposition = require('../models/BodyComposition');
 const { AssessmentService } = require('../services/assessmentService');
-const { PatientHistoryService } = require('../services/patientHistoryService');
+const PatientHistoryService = require('../services/PatientHistoryService');
 
 
 // Get all assessments with associated data
