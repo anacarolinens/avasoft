6feeb61124274d7feaf97f601c85a7f8e6f3a91e
Anderson Dantas Santos <netodantas3@gmail.com>
2024-10-21 13:12:20 -0500

    feat:patient page settings

--- landingpage/src/components/PainelPatient.vue
+++ landingpage/src/components/PainelPatient.vue
@@ -40,7 +40,7 @@
       };
     },
     mounted() {
-      this.renderChart();
+      
     },
   };
   </script>
--- landingpage/src/views/HomePatientView.vue
+++ landingpage/src/views/HomePatientView.vue
@@ -1,25 +1,43 @@
 <template>
-  <div class="justify-center mx-auto bg-[#1E2021] ">
+  <div class="justify-center mx-auto bg-[#1E2021] min-h-screen flex flex-col">
     <HeaderPatient/>
-    <main class="py-4 px-2 max-w-7xl mb-12 mx-auto">
+    <main class="flex flex-1 py-4 px-2 max-w-7xl mb-12 mx-auto">
       <div class="relative isolate overflow-hidden">
-        <div class="mx-auto justify-center lg:flex flex-col items-center">
-          
+        <SidebarPatient @menu-selected="handleMenuSelected" :isOpen="isSidebarOpen" />
+        <div class="flex-1">
+          <PainelPatient v-if="selectedMenu === 'painel'" />
         </div>
       </div>
     </main>
     <FooterPatient/>
   </div>
-  </template>
-  
+</template>
+
 <script setup>
+import { ref } from 'vue';
 import FooterPatient from '../components/FooterPatient.vue';
 import HeaderPatient from '../components/HeaderPatient.vue';
+import SidebarPatient from '../components/SideBarPatient.vue';
+import PainelPatient from '../components/PainelPatient.vue';
 
+const selectedMenu = ref('painel');
+const isSidebarOpen = ref(false);
 
-
+function handleMenuSelected(menu) {
+  selectedMenu.value = menu;
+}
 </script>
-  
+
 <style scoped>
- </style>
-  
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
