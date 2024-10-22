79154f07fd983e176f028fcb2c4b99ef077195b3
Ana Caroline <85416744+anacarolinens@users.noreply.github.com>
2024-10-21 22:18:41 -0500

    :sparkles: feat: update patient page configurations

--- landingpage/src/components/ConfigPatient.vue
+++ landingpage/src/components/ConfigPatient.vue
@@ -1,6 +1,125 @@
 <template>
+    <div class="container flex flex-col justify-start min-h-screen text-white px-4">
+      
+      <!-- Título do formulário -->
+      <h1 class="text-3xl mb-8 mt-10 ">CONFIGURAÇÕES</h1>
+     
+        <!-- Formulário de configurações -->
+        <form @submit.prevent="handleRegister" id="register-form" class="w-full max-w-4xl text-black">
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <!-- Nome completo -->
+            <div class="flex flex-col mx-4">
+              <label for="fullName" class="mb-2 text-white">Nome completo</label>
+              <input type="text" id="fullName" placeholder="Digite seu nome completo aqui..." v-model="fullName"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- CPF -->
+            <div class="flex flex-col mx-4">
+              <label for="cpf" class="mb-2 text-white">CPF</label>
+              <input type="text" id="cpf" placeholder="Digite seu CPF aqui..." v-model="cpf"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- Telefone -->
+            <div class="flex flex-col mx-4">
+              <label for="phone" class="mb-2 text-white">Telefone</label>
+              <input type="text" id="phone" placeholder="Digite seu telefone aqui..." v-model="phone"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- Email -->
+            <div class="flex flex-col mx-4">
+              <label for="email" class="mb-2 text-white">Email</label>
+              <input type="email" id="email" placeholder="Digite seu email aqui..." v-model="email"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- Nome de Usuário -->
+            <div class="flex flex-col mx-4">
+              <label for="userName" class="mb-2 text-white">Nome de Usuário</label>
+              <input type="text" id="userName" placeholder="Digite seu nome de usuário aqui..." v-model="userName"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- Rua -->
+            <div class="flex flex-col mx-4">
+              <label for="street" class="mb-2 text-white">Rua</label>
+              <input type="text" id="street" placeholder="Digite sua rua aqui..." v-model="street"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- Número -->
+            <div class="flex flex-col mx-4">
+              <label for="number" class="mb-2 text-white">Número</label>
+              <input type="number" id="number" placeholder="Digite o número aqui..." v-model="number"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- Complemento -->
+            <div class="flex flex-col mx-4">
+              <label for="complement" class="mb-2 text-white">Complemento</label>
+              <input type="text" id="complement" placeholder="Digite o complemento aqui..." v-model="complement"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- Bairro -->
+            <div class="flex flex-col mx-4">
+              <label for="district" class="mb-2 text-white">Bairro</label>
+              <input type="text" id="district" placeholder="Digite seu bairro aqui..." v-model="district"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- Cidade -->
+            <div class="flex flex-col mx-4">
+              <label for="city" class="mb-2 text-white">Cidade</label>
+              <input type="text" id="city" placeholder="Digite sua cidade aqui..." v-model="city"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- Estado -->
+            <div class="flex flex-col mx-4">
+              <label for="state" class="mb-2 text-white">Estado</label>
+              <input type="text" id="state" placeholder="Digite seu estado aqui..." v-model="state"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- CEP -->
+            <div class="flex flex-col mx-4">
+              <label for="cep" class="mb-2 text-white">CEP</label>
+              <input type="text" id="cep" placeholder="Digite seu CEP aqui..." v-model="cep"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- Senha Atual -->
+            <div class="flex flex-col mx-4">
+              <label for="currentPassword" class="mb-2 text-white">Senha Atual</label>
+              <input type="password" id="currentPassword" placeholder="Digite sua senha atual aqui..." v-model="currentPassword"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+    
+            <!-- Nova Senha -->
+            <div class="flex flex-col mx-4">
+              <label for="newPassword" class="mb-2 text-white">Nova Senha</label>
+              <input type="password" id="newPassword" placeholder="Digite sua nova senha aqui..." v-model="newPassword"
+                class="w-full p-3 rounded focus:border-orange-500 bg-input-color bg-[#3E3E3E]">
+            </div>
+          </div>
+    
+        <!-- Botões -->
+          <div class="flex flex-col sm:flex-row justify-end sm:space-x-4 mt-10 mx-4 space-y-4 sm:space-y-0">
+            <button type="button" class="w-full sm:w-auto min-w-[200px] h-12 px-4 py-3 bg-transparent text-white border border-white rounded">
+              Limpar
+            </button>
+            <button type="submit" class="w-full sm:w-auto min-w-[200px] h-12 bg-[#FF8139] text-white p-3 rounded">
+              Salvar
+            </button>
+          </div>
 
-</template>
+        </form>
+      </div>
+ 
+  </template>
 
 <script>
 
--- landingpage/src/components/PainelPatient.vue
+++ landingpage/src/components/PainelPatient.vue
@@ -1,42 +1,41 @@
 <template>
-    <div class="flex flex-col items-center justify-center text-white">
-      <!-- Título de boas-vindas -->
-      <h1 class="text-2xl mb-8">Bem-vindo(a) Fernanda!</h1>
-  
-      <!-- Seção do gráfico -->
-      <div class="max-w-4xl p-4 rounded-lg">
-        <!-- Botões de navegação para os gráficos -->
-         <p class="mt-5">GRÁFICO</p>
-        <div class="flex justify-around mb-4">
-          <button
-            v-for="(tab, index) in tabs"
-            :key="index"
-            class="bg-orange-500 text-white px-6 py-2 mr-2 mt-8 rounded-lg hover:bg-orange-600 focus:outline-none"
-            @click="activeTab = index"
-          >
-            {{ tab }}
-          </button>
+  <div class="flex flex-col items-center text-white mt-10">
+    <!-- Título de boas-vindas -->
+    <h1 class="text-3xl mb-12 ">Bem-vindo(a) Fernanda!</h1>
+
+    <!-- Seção do gráfico -->
+    <div class="max-w-4xl p-4 rounded-lg ml-30 min-w-full">
+      <!-- Botões de navegação para os gráficos -->
+      <p class="mb-5 text-2xl">GRÁFICO</p>
+      <div class="flex flex-col sm:flex-row space-y-3 sm:space-y-0 sm:space-x-3">
+        <Button
+          v-for="(tab, index) in tabs"
+          :key="index"
+          class="w-full sm:flex-1 min-w-[200px] h-12 bg-[#FF8139] px-6 py-3 text-white rounded-md hover:bg-[#FF5C00] text-center mb-2"
+          @click="activeTab = index"
+        >
+          {{ tab }}
+        </Button>
+      </div>
+
+
+      <!-- Gráfico -->
+      <div class="bg-[#3E3E3E] p-4 rounded-lg min-w-full">
+        <div class="relative p-16">
+          <canvas id="chart"></canvas>
         </div>
-  
-        <!-- Gráfico -->
-        <div class="bg-[#3E3E3E] p-4 rounded-lg">
-          <div class="relative">
-            <!-- Placeholder do gráfico, você pode integrar um gráfico real aqui -->
-            <canvas id="chart"></canvas>
-          </div>
-          <p class="text-right text-xs mt-2">03/04/2024</p>
-        </div>
+        <p class="text-right text-xs mt-2">03/04/2024</p>
       </div>
     </div>
-  </template>
-  
-  <script>
+  </div>
+</template>
+<script>
+import Button from './Button.vue';
   export default {
     data() {
       return {
-        // Tabs para os diferentes tipos de gráficos
-        tabs: ['Peso', 'IMC', 'Pregas Cutâneas', 'Circunferências', 'Bioimpedância'],
-        activeTab: 0, // Aba ativa
+        tabs: ['Peso', 'IMC', 'Pregas Cutâneas', 'Circunferências'],
+        activeTab: 0, 
       };
     },
     mounted() {
