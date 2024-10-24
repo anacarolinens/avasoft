f7f9da648ebc4db7395823cd6cdf3cab5f498437
Felipe-dSantos <lipehh18@gmail.com>
2024-10-24 00:45:20 -0300

    feat: update EditAssessment.vue

--- frontend/src/templates/EditAssessment.vue
+++ frontend/src/templates/EditAssessment.vue
@@ -4,144 +4,86 @@
         <h3 class="font-bold text-gray-800" v-if="selectedAssessment">
             Edição da Avaliação {{ selectedAssessment.id_assessment }}
         </h3>
+        <div class="w-2/3">
 
-        <div v-if="selectedAssessment" class="p-4">
-            <form @submit.prevent="updateAssessment">
-                <div class="grid grid-cols-2 gap-4">
+            <div v-if="selectedAssessment" class="p-4">
+                <form @submit.prevent="updateAssessment">
                     <label for="assessmentDate">Data da Avaliação:</label>
                     <p>{{ formatDate(selectedAssessment.assessmentDate) }}</p>
-                    <!-- <label for="gender">Sexo:</label>
-                    <select v-model="selectedAssessment.gender" id="gender" class="border p-2 rounded" required>
-                        <option value="" disabled selected>Selecione o sexo</option>
-                        <option value="Masculino">Masculino</option>
-                        <option value="Feminino">Feminino</option>
-                        <option value="Outro">Outro</option>
-                    </select>
+                    <div class="mb-4 flex space-x-4">
+                        <div class="w-1/2">
+                            <label for="weight" class="block text-sm font-medium text-gray-700">Peso (kg)</label>
 
-                    <label for="age">Idade (anos):</label>
-                    <input type="number" v-model="selectedAssessment.age" id="age" class="border p-2 rounded"
-                        required /> -->
-                    <label for="weight">Peso (kg):</label>
-                    <input type="number" v-model="selectedAssessment.weight" id="weight" class="border p-2 rounded"
-                        required />
+                            <input class="mt-1 p-2 w-full border-gray-300 rounded-md" type="number"
+                                v-model="selectedAssessment.weight" id="weight" required />
+                        </div>
+                        <div class="w-1/2">
+                            <label for="altura" class="block text-sm font-medium text-gray-700">Altura (cm)</label>
 
-                    <label for="height">Altura (cm):</label>
-                    <input type="number" v-model="selectedAssessment.height" id="height" class="border p-2 rounded"
-                        required />
+                            <input type="number" v-model="selectedAssessment.height" id="height"
+                                class="mt-1 p-2 w-full border-gray-300 rounded-md" required />
+                        </div>
+                    </div>
+                    <!-- Seleção do Método de Avaliação -->
+                    <div class="mb-4">
+                        <label for="method" class="block text-sm font-medium text-gray-700" >Método de
+                            Avaliação</label>
+                        <select v-model="selectedAssessment.method" id="method"
+                            class="mt-1 p-2 w-full border-gray-300 rounded-md" disabled >
+                            <option value="" disabled selected>Selecione o método</option>
+                            <option value="Guedes">Guedes</option>
+                            <option value="Pollock" disabled>Pollock</option>
+                            <option value="Jackson & Pollock" disabled>Jackson & Pollock</option>
+                            <option value="McArdle">McArdle</option>
+                            <option value="Dados Livres">Dados Livres</option>
+                        </select>
+                    </div>
+                    <!-- Dobras Cutâneas -->
+                    <legend class="text-lg font-medium text-gray-700">Dobras Cutâneas (mm)</legend>
+                    <div v-if="selectedAssessment && selectedAssessment.skinfold"  class="grid grid-cols-3 gap-4">
+                        <div v-for="(value, key) in skinfoldData" :key="key">
+                            <label :for="key" class="block text-sm font-medium">{{ traduzirDobrasCutaneas(key)
+                                }}</label>
+                            <input v-model="selectedAssessment.skinfold[key]"
+                                :disabled="!camposNecessarios.includes(key)"
+                                :class="{ 'disabled-input': !camposNecessarios.includes(key) }" type="number"
+                                class="mt-1 p-2 w-full border-gray-300 rounded-md" />
+                        </div>
+                    </div>
 
-                    <label for="method">Método:</label>
-                    <select v-model="selectedAssessment.method" id="method" class="border p-2 rounded" required>
-                        <option value="" disabled selected>Selecione o método</option>
-                        <option value="Guedes">Guedes</option>
-                        <option value="Pollock" disabled>Pollock</option>
-                        <option value="Jackson & Pollock" disabled>Jackson & Pollock</option>
-                        <option value="McArdle">McArdle</option>
-                        <option value="Dados Livres">Dados Livres</option>
-                    </select>
-                </div>
+                    <!-- Circunferências -->
+                    <legend class="text-lg font-medium text-gray-700">Circunferências (cm)</legend>
+                    <div v-if="selectedAssessment && selectedAssessment.skinfold" class="grid grid-cols-3 gap-4">
+                        <div v-for="(value, key) in circumferenceData" :key="key">
+                            <label :for="key" class="block text-sm font-medium">{{ traduzirCircunferencia(key)
+                                }}</label>
+                            <input v-model="selectedAssessment.circumference[key]"
+                                :disabled="!camposNecessarios.includes(key)"
+                                :class="{ 'disabled-input': !camposNecessarios.includes(key) }" type="number"
+                                class="mt-1 p-2 w-full border-gray-300 rounded-md" />
+                        </div>
+                    </div>
 
-                <h4 class="my-4 font-bold text-black">Circunferências (cm)</h4>
-                <div class="grid grid-cols-2 gap-4">
-                    <!-- Entradas para circunferências -->
-                    <label for="neck">Pescoço:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.neck" id="neck"
-                        class="border p-2 rounded" />
 
-                    <label for="thorax">Tórax:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.thorax" id="thorax"
-                        class="border p-2 rounded" />
 
-                    <label for="waist">Cintura:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.waist" id="waist"
-                        class="border p-2 rounded" />
-
-                    <label for="abdomen">Abdômen:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.abdomen" id="abdomen"
-                        class="border p-2 rounded" />
-
-                    <label for="hip">Quadril:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.hip" id="hip"
-                        class="border p-2 rounded" />
-
-                    <label for="leftArm">Braço Esquerdo:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.leftArm" id="leftArm"
-                        class="border p-2 rounded" />
-
-                    <label for="rightArm">Braço Direito:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.rightArm" id="rightArm"
-                        class="border p-2 rounded" />
-
-                    <label for="leftForearm">Antebraço Esquerdo:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.leftForearm" id="leftForearm"
-                        class="border p-2 rounded" />
-
-                    <label for="rightForearm">Antebraço Direito:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.rightForearm" id="rightForearm"
-                        class="border p-2 rounded" />
-
-                    <label for="leftGlutealThigh">Coxa Esquerda:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.leftGlutealThigh"
-                        id="leftGlutealThigh" class="border p-2 rounded" />
-
-                    <label for="rightGlutealThigh">Coxa Direita:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.rightGlutealThigh"
-                        id="rightGlutealThigh" class="border p-2 rounded" />
-
-                    <label for="leftLeg">Perna Esquerda:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.leftLeg" id="leftLeg"
-                        class="border p-2 rounded" />
-
-                    <label for="rightLeg">Perna Direita:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.rightLeg" id="rightLeg"
-                        class="border p-2 rounded" />
-
-                    <label for="leftWrist">Punho Esquerdo:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.leftWrist" id="leftWrist"
-                        class="border p-2 rounded" />
-
-                    <label for="rightWrist">Punho Direito:</label>
-                    <input type="number" v-model="selectedAssessment.circumference.rightWrist" id="rightWrist"
-                        class="border p-2 rounded" />
-                </div>
-
-                <h4 class="my-4 font-bold text-black">Dobras Cutâneas (mm)</h4>
-                <div class="grid grid-cols-2 gap-4">
-                    <label for="triceps">Tríceps:</label>
-                    <input type="number" v-model="selectedAssessment.skinfold.triceps" id="triceps"
-                        class="border p-2 rounded" />
-
-                    <label for="abdominal">Abdominal:</label>
-                    <input type="number" v-model="selectedAssessment.skinfold.abdominal" id="abdominal"
-                        class="border p-2 rounded" />
-
-                    <label for="subscapular">Subescapular:</label>
-                    <input type="number" v-model="selectedAssessment.skinfold.subscapular" id="subscapular"
-                        class="border p-2 rounded" />
-
-                    <label for="suprailiac">Supra-ilíaca:</label>
-                    <input type="number" v-model="selectedAssessment.skinfold.suprailiac" id="suprailiac"
-                        class="border p-2 rounded" />
-
-                    <label for="thigh">Coxa:</label>
-                    <input type="number" v-model="selectedAssessment.skinfold.thigh" id="thigh"
-                        class="border p-2 rounded" />
-                </div>
-
-                <div class="flex justify-end items-center gap-x-2 py-3">
-                    <button type="submit" class="py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
-                        Salvar Alterações
-                    </button>
-                    <button @click="goBack" type="button"
-                        class="py-2 px-4 bg-gray-500 text-white rounded-lg hover:bg-gray-600">
-                        Cancelar
-                    </button>
-                </div>
-            </form>
+                    <div class="flex justify-end items-center gap-x-2 py-3">
+                        <button type="submit" class="py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+                            Salvar Alterações
+                        </button>
+                        <button @click="goBack" type="button"
+                            class="py-2 px-4 bg-gray-500 text-white rounded-lg hover:bg-gray-600">
+                            Cancelar
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <div v-else>
+                <p>Carregando dados da avaliação...</p>
+            </div>
         </div>
 
-        <div v-else>
-            <p>Carregando dados da avaliação...</p>
-        </div>
+
+
     </div>
 </template>
 
@@ -155,19 +97,125 @@ export default {
     },
     data() {
         return {
-            selectedAssessment: null,
+            selectedAssessment: {},
             showToast: false,
             toastMessage: '',
             toastType: '',
+            camposNecessarios: [],
+            circumferenceData: {
+                neck: null,
+                thorax: null,
+                shoulderBlade: null,
+                waist: null,
+                abdomen: null,
+                hip: null,
+                leftWrist: null,
+                rightWrist: null,
+                leftArm: null,
+                rightArm: null,
+                leftContractedArm: null,
+                rightContractedArm: null,
+                leftForearm: null,
+                rightForearm: null,
+                leftGlutealThigh: null,
+                rightGlutealThigh: null,
+                leftMedialThigh: null,
+                rightMedialThigh: null,
+                leftLeg: null,
+                rightLeg: null,
+                leftAnkle: null,
+                rightAnkle: null,
+            },
+            skinfoldData: {
+                triceps: null,
+                axillary: null,
+                abdominal: null,
+                thigh: null,
+                calf: null,
+                subscapular: null,
+                suprailiac: null,
+                pectoral: null,
+                bicep: null,
+            },
         };
     },
     methods: {
+        traduzirCircunferencia(key) {
+            const traducoes = {
+                neck: 'Pescoço',
+                thorax: 'Tórax',
+                shoulderBlade: 'Ombro',
+                waist: 'Cintura',
+                abdomen: 'Abdômen',
+                hip: 'Quadril',
+                leftWrist: 'Pulso Esquerdo',
+                rightWrist: 'Pulso Direito',
+                leftArm: 'Braço Esquerdo',
+                rightArm: 'Braço Direito',
+                leftContractedArm: 'Braço Contraído Esquerdo',
+                rightContractedArm: 'Braço Contraído Direito',
+                leftForearm: 'Antebraço Esquerdo',
+                rightForearm: 'Antebraço Direito',
+                leftGlutealThigh: 'Coxa Glútea Esquerda',
+                rightGlutealThigh: 'Coxa Glútea Direita',
+                leftMedialThigh: 'Coxa Medial Esquerda',
+                rightMedialThigh: 'Coxa Medial Direita',
+                leftLeg: 'Perna Esquerda',
+                rightLeg: 'Perna Direita',
+                leftAnkle: 'Tornozelo Esquerdo',
+                rightAnkle: 'Tornozelo Direito',
+            };
+            return traducoes[key] || key;
+
+        },
+        traduzirDobrasCutaneas(key) {
+            const traducoes = {
+                triceps: 'Tríceps',
+                axillary: 'Axilar',
+                abdominal: 'Abdominal',
+                thigh: 'Coxa',
+                calf: 'Panturrilha',
+                subscapular: 'Subescapular',
+                suprailiac: 'Suprailíaca',
+                pectoral: 'Peitoral',
+                bicep: 'Bíceps',
+            };
+            return traducoes[key] || key;
+        },
+        atualizarCamposNecessarios() {
+            const metodo = this.selectedAssessment.method;
+            if (metodo === 'Guedes') {
+                this.camposNecessarios = ['triceps', 'suprailiac', 'abdominal'];
+            } else if (metodo === 'Pollock') {
+                this.camposNecessarios = ['triceps', 'suprailiac', 'abdominal', 'thigh', 'pectoral'];
+            } else if (metodo === 'Jackson & Pollock') {
+                this.camposNecessarios = ['triceps', 'subscapular', 'axillary', 'abdominal', 'thigh', 'suprailiac', 'pectoral'];
+            } else if (metodo === 'McArdle') {
+                this.camposNecessarios = ['triceps', 'subscapular', 'suprailiac', 'abdominal', 'thigh'];
+            } else if (metodo === 'Dados Livres') {
+                this.camposNecessarios = [
+                    ...Object.keys(this.selectedAssessment.skinfold || {}),
+                    ...Object.keys(this.selectedAssessment.circumference || {})
+                ];
+            } else {
+                this.camposNecessarios = [
+                    ...Object.keys(this.selectedAssessment.skinfold || {}),
+                    ...Object.keys(this.selectedAssessment.circumference || {})
+                ];
+            }
+        },
+
         async fetchAssessmentById() {
             try {
                 const id_assessment = this.$route.params.id;
-                console.log('ID da avaliação:', id_assessment);// Assumindo que a rota contém o ID
+                console.log('ID da avaliação:', id_assessment);
                 const response = await axios.get(`http://localhost:3000/assessments/${id_assessment}`);
                 this.selectedAssessment = response.data;
+                // Atualiza os campos necessários após carregar os dados da avaliação
+                this.atualizarCamposNecessarios();
+                this.selectedAssessment = response.data;
+        this.selectedAssessment.skinfold = this.selectedAssessment.skinfold || {};
+        this.selectedAssessment.circumference = this.selectedAssessment.circumference || {};
             } catch (error) {
                 console.error('Erro ao buscar avaliação:', error);
             }
@@ -221,7 +269,7 @@ export default {
                 this.showToast = true;
                 this.toastMessage = 'Avaliação atualizada com sucesso!';
                 this.toastType = 'success';
-             // Redirecionar após a atualização
+                // Redirecionar após a atualização
             } catch (error) {
                 console.error('Erro ao atualizar avaliação:', error);
                 this.showToast = true;
@@ -237,7 +285,11 @@ export default {
             return new Date(date).toLocaleDateString('pt-BR', options);
         }
     },
+    watch: {
+        'selectedAssessment.method': 'atualizarCamposNecessarios',
+    },
     mounted() {
+        this.atualizarCamposNecessarios();
         this.fetchAssessmentById();
     },
 };
